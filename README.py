2834a67178a8c43ffac33f48afe93ade468339df
TigranDan998 <63162351+TigranDan998@users.noreply.github.com>
2020-04-14 20:34:26 +0400

    Update README.py

--- README.py
+++ README.py
@@ -20,3 +20,25 @@ else:
 #Задача 3
 number = input('Введите число ') #например 58375
 print(max(number))
+
+#homework_leson3_easy
+#задача 1
+fruits=['apple','banan','mango','orange']
+fruit=len(fruits)
+for i in range(fruit):
+    print(str(i+1)+'.'+'{}'.format(fruits[i]))
+#задача 2
+a = {'1', '2', '3', '4', '5', '6'}
+b = {'1', '2', '3', '4'}
+c = a - b
+print(list(c))
+#задача 3
+one_list=[2,3,4,5,6,8,10,12]
+new_list=[]
+two_list=len(one_list)
+for i in range(two_list):
+    if one_list[i]%2==0:
+        new_list.append(one_list[i]/4)
+    else:
+        new_list.append(one_list[i]*2)
+print(new_list)
